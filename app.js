e902ee9dbabebe1d0fcc03db0b686ebc411d329a
claurennt <c.benedetti1989@gmail.com>
2021-02-28 15:27:09 +0100

    update project with node and npm

---
+++ app.js
@@ -0,0 +1,21 @@
+const express= require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
