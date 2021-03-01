c5af3ca004725273624c871b90bb4cc26e93ce30
claurennt <c.benedetti1989@gmail.com>
2021-03-01 17:37:53 +0100

    update project with more routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express= require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 
+const _ = require('lodash');
+
 
 
 const app = express();
@@ -16,6 +18,22 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/prodotti', (req, res) => {
+  res.render('prodotti');
+});
+
+app.get('/storia', (req, res) => {
+  res.render('storia');
+});
+
+app.get('/contatti', (req, res) => {
+  res.render('contatti');
+});
+
+app.get('/prodotti/canottiere-donna', (req, res) => {
+  res.render('canottiere-donna');
 });
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'));
