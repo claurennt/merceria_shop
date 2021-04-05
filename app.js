e2e4c623642d9fecc7c31c00e9645f1b24db8de2
claurennt <c.benedetti1989@gmail.com>
2021-04-05 19:05:12 +0200

    update canottiere page and navbar

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
 const fetch = require('node-fetch');
-const mongoose = require('mongoose')
 const weather = require(__dirname + '/weather.js')
 
 // const _ = require('lodash');
@@ -13,50 +12,33 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
+
 app.get('/', (req, res) => {
 //use imported getWeather bound to const weather and then render index passing in the key value paths for the partials 'header'
-  weather.getWeather().then((result) => {
-    res.render('index', {
-
-      //results is the weather object returned from getWeather(), thu we can call the same properties and assign them to the key value paths
-      city:result.city,
-      icon: result.icon,
-      temperature: result.temperature
-    })
+  weather.getWeather().then((weatherData) => {
+    res.render('index', { weatherData: weatherData })
   });
 
 });
 
 
 app.get('/prodotti', (req, res) => {
-  weather.getWeather().then((result) => {
-    res.render('prodotti', {
-      city: result.city,
-      icon: result.icon,
-      temperature: result.temperature
-    })
+  weather.getWeather().then((weatherData) => {
+    res.render('prodotti', { weatherData: weatherData })
   });
 
 });
 
 app.get('/storia', (req, res) => {
-  weather.getWeather().then((result) => {
-    res.render('storia', {
-      city: result.city,
-      icon: result.icon,
-      temperature: result.temperature
-    })
+  weather.getWeather().then((weatherData) => {
+    res.render('storia', { weatherData: weatherData })
   });
 
 });
 
 app.get('/contatti', (req, res) => {
-  weather.getWeather().then((result) => {
-    res.render('contatti', {
-      city: result.city,
-      icon: result.icon,
-      temperature: result.temperature
-    })
+  weather.getWeather().then((weatherData) => {
+    res.render('contatti', { weatherData: weatherData })
   });
 
 });
@@ -64,13 +46,8 @@ app.get('/contatti', (req, res) => {
 app.get('/prodotti/:productName', (req, res) => {
   const productName = req.params.productName
 
-  weather.getWeather().then((result) => {
-    res.render(productName, {
-      city: result.city,
-      icon: result.icon,
-      temperature: result.temperature,
-
-    })
+weather.getWeather().then((weatherData) => {
+    res.render(productName, { weatherData: weatherData })
 
 });
 });
--- weather.js
+++ weather.js
@@ -7,18 +7,8 @@ const apiKey = process.env.OPENWEATHER_KEY;
 //use async/await to fetch weahter API
 async function getWeather() {
   const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Riccione&appid=' + apiKey + '&units=metric')
-  const data = await res.json();
-  const city = data.name
-  let icon = data.weather[0].icon
-  let temp = Math.round(data.main.temp)
-//create an object with parsed datas
-  let weather = {
-    city: city,
-    icon: 'http://openweathermap.org/img/wn/' + icon + '@2x.png',
-    temperature: temp + '°'
-  }
-
-  return weather;
+  const weatherData = await res.json();
+  return weatherData;
 }
 
 module.exports = {
